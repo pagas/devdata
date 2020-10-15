981390b3a962a65b9f5c0192879ae3770b9332b5
paulius <paulius@2simple.com>
2020-10-15 21:12:36 +0100

    add api and connect client with api

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [apiResponse, setApiResponse] = useState(0);
+
+    useEffect(()=> {
+        callAPI();
+        return () => {
+
+        }
+    }, [])
+
+    function callAPI() {
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => setApiResponse(res));
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+
+
+                API Response : {apiResponse}
+            </header>
+        </div>
+    );
 }
 
 export default App;
