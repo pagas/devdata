edacde5194644db9b269c1779ac738e04fac04b9
paulius <paulius@2simple.com>
2020-11-16 22:24:34 +0000

    fix logo text

--- client/src/App.js
+++ client/src/App.js
@@ -49,7 +49,7 @@ const App = () => {
             <div>
                 <nav className="navbar navbar-expand navbar-dark bg-dark">
                     <Link to={"/"} className="navbar-brand">
-                        bezKoder
+                        DevData
                     </Link>
                     <div className="navbar-nav mr-auto">
                         <li className="nav-item">
