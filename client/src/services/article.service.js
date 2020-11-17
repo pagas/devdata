d0f0b1db425b13cb8d6c54d70071c005d8d06d22
paulius <paulius@2simple.com>
2020-11-17 13:08:40 +0000

    create article service

---
+++ client/src/services/article.service.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import authHeader from "./auth-header";
+import {API_ROOT} from "../api-config";
+
+const API_URL = `${API_ROOT}/article/`;
+
+const getAllArticles = () => {
+    return axios.get(`${API_URL}all`);
+}
+
+const getArticleById = (id) => {
+    return axios.get(`${API_URL}view/${id}`, {headers: authHeader()});
+}
+
+const createArticle = (title, body) => {
+    return axios.post(`${API_URL}create`, {
+        title,
+        body,
+        headers: authHeader()
+    });
+}
+
+
+export default {
+    getAllArticles,
+    getArticleById,
+    createArticle
+}
